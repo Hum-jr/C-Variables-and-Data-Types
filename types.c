4b656bf0fd52b430e648b9c9b32bfbee32f55460
Hum-jr <bennyangiye202@gmail.com>
2024-02-20 18:52:38 +0300

    understanding types

---
+++ types.c
@@ -0,0 +1,51 @@
+//
+// Created by humph on 2/20/2024.
+//
+
+//Data types
+//Integer
+//  -signed and unsigned
+//  -short and long
+//  -fixed-width integer types
+//  -#include <stdint.h>
+//  -#inlude <inttypes.h> (C99)
+
+//Char
+//  -char,unsigned char,signed char
+//  -A=65, Z=90
+//  -Relies on the ASCII code
+
+//Boolean
+//  -Added in the C99 standard
+//  -#include <stdbool.h>
+
+//Enumeration
+
+//Bit
+//  -unsigned int age: 7;
+
+//Void
+
+//Printing and reading integer types
+//We use format specifiers
+
+//Format
+//%d
+
+//to use the complex numbers u need to import the complex header complex.h
+
+//Derived data types
+//Arrays
+//Reps a collection of values with the same type and rep by a single name.
+//For instance int arr[5]={1,2,3,4,5}
+//Arrays have dimensions - 2D,3D...
+
+//Using pointers
+
+#include <stdio.h>
+
+int main(){
+    float x = 3.0;
+    printf("%f",x);
+    return 0;
+}
